01e1ac042c56414aa124767581085c65fb9415f3
杨淞 <443031861@qq.com>
2023-12-23 13:37:33 +0800

    3

---
+++ POE Auxiliary Tools/BaseData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POE_Auxiliary_Tools
+{
+    public class UserAgent
+    {
+        public string agent { get; set; }
+    }
+}
--- POE Auxiliary Tools/Program.cs
+++ POE Auxiliary Tools/Program.cs
@@ -11,6 +11,7 @@ namespace POE_Auxiliary_Tools
     internal static class Program
     {
         public static 用户信息Mode baseInfo = new 用户信息Mode();
+        public static List<UserAgent> userAgentList = new List<UserAgent>();
         /// <summary>
         /// 应用程序的主入口点。
         /// </summary>
@@ -25,6 +26,15 @@ namespace POE_Auxiliary_Tools
                 Application.SetCompatibleTextRenderingDefault(false);
                 Application.Run(new MainFrom());
             }
+        }
+        private static void Init()
+        {
+            Program.userAgentList.Add(new UserAgent() { agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0" });
+            Program.userAgentList.Add(new UserAgent() { agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36" });
+            Program.userAgentList.Add(new UserAgent() { agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0" });
+            Program.userAgentList.Add(new UserAgent() { agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15" });
+            Program.userAgentList.Add(new UserAgent() { agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.37" });
+            Program.userAgentList.Add(new UserAgent() { agent = "Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko" });
         }
     }
 }
